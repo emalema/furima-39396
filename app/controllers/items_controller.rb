4ec0c8b3296421d1ba81b0c9b76e0ccc6e9f7e5f
emalema <eyemalema5@gmail.com>
2023-09-23 01:22:07 +0900

    モデル・テーブル作成〜ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,18 @@
 class ItemsController < ApplicationController
-  def index; end
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :image).merge(user_id: current_user.id)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+
+  # Associations
+  has_many :items
 
   # Constants
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
