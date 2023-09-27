335bd1303d4939abfec9499c97f586695be1460d
emalema <eyemalema5@gmail.com>
2023-09-28 01:18:02 +0900

    商品出品ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
-    @items = Item.all
+    @items = Item.order("created_at DESC")
   end
 
   def new
@@ -8,11 +10,27 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+def item_params
+  params.require(:item).permit(
+    :name,
+    :image,
+    :price,
+    :description,
+    :category_id,
+    :item_condition_id,
+    :shipping_fee_id,
+    :prefecture_id,
+    :shipping_date_id
+  ).merge(user_id: current_user.id)
   end
 end
 
-private
-
-def item_params
-  params.require(:item).permit(:name, :image).merge(user_id: current_user.id)
-end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_date
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  validates :name, :description, :image, presence: true
+  validates :category_id, :item_condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" }
 end
