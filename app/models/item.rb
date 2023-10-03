dbda41ea2b42945090b4e33596c42b47d9400459
emalema <eyemalema5@gmail.com>
2023-10-03 10:19:34 +0900

    仮commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,15 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
-  validates :name, :description, :image, presence: true
-  validates :category_id, :item_condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :image, :name, :description, presence: true
+  validates :price, presence: true,
+                    # numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "must be between ¥300 to ¥9,999,999"},
+                    format: { with: /\A[0-9]+\z/, message: "Price must be a half-width number" }
+
+  validates :category_id, :item_condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id,
+              numericality: { other_than: 1 , message: "can't be blank" }
+
+  def was_attached?
+    self.image.attached?
+  end
 end
