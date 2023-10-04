567bc1c3e75b0b350978e0ff1dc16fe9e68e2b3f
emalema <eyemalema5@gmail.com>
2023-10-04 14:57:30 +0900

    レビューによる修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Item, type: :model do
   describe '#create' do
     before do
       @item = FactoryBot.build(:item)
-      @item.image = fixture_file_upload('app/assets/images/item-sample.png')
     end
 
     context '商品出品ができる時' do
@@ -69,6 +68,11 @@ RSpec.describe Item, type: :model do
         @item.price = '３００'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be a half-width number')
+      end
+      it 'ユーザーが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
