b2b00e4b4b9b6720f5560142ccb944c03d57dd04
emalema <eyemalema5@gmail.com>
2023-10-10 17:03:41 +0900

    レビューによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,8 @@ class ItemsController < ApplicationController
   def show; end
 
   def destroy
-    if @item.user_id == current_user.id && @item.destroy
+    if @item.user_id == current_user.id
+      @item.destroy
       redirect_to root_path
     else
       render :show
