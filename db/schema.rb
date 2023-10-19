f897fa446ce4c5f0af282974ef330f83590e0b79
emalema <eyemalema5@gmail.com>
2023-10-19 12:51:28 +0900

    デプロイ失敗によるカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_13_061724) do
+ActiveRecord::Schema.define(version: 2023_10_19_034843) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2023_10_13_061724) do
     t.string "first_name_kana"
     t.string "last_name_kana"
     t.datetime "date_of_birth"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
